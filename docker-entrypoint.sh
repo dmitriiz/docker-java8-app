f6e823ed143c21478d441c663c23aed94cf63604
Dmitry Zapashchikov <dmitry.zapashchikov@itrespublica.eu>
2016-09-07 17:16:17 +0300

    APP_NAME environment variable added to be mapped as user.app property

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.security.egd=file:/dev/./urandom -Duser.timezone=$APP_TZ -Duser.language=$APP_LANG -Duser.country=$APP_COUNTRY -jar /application.jar $@
+java -Duser.app=$APP_NAME -Duser.timezone=$APP_TZ -Duser.language=$APP_LANG -Duser.country=$APP_COUNTRY -Djava.security.egd=file:/dev/./urandom -jar /application.jar $@
